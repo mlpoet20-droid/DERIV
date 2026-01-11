5fde8c3e8c40a2b16a09843f91557c353d782597
mlpoet20-droid <mlpoet2.0@gmail.com>
2026-01-11 14:39:04 +0200

    Delete deri_synthetics_app directory

--- deri_synthetics_app/lib/models/trade.dart
+++
@@ -1,9 +0,0 @@
-class Trade {
-  String symbol;
-  double amount;
-  String contractType;
-  int duration;
-  double entryIV;
-
-  Trade({required this.symbol, required this.amount, required this.contractType, required this.duration, required this.entryIV});
-}
